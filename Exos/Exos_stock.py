34818b89fb5206b0b5b628f9b0e6976dc7d1f35c
Julosse27 <lateurte.jules@gmail.com>
2025-09-16 20:40:24 +0200

    Update Exos_stock.py

--- Exos/Exos_stock.py
+++ Exos/Exos_stock.py
@@ -4,7 +4,7 @@ class Tuto_1(discord.ui.View):
     def __init__(self, *,  timeout: float | None = None):
         super().__init__(timeout=timeout)
 
-    @discord.ui.button(label= "Testez moi!", style= discord.ButtonStyle.red)
+    @discord.ui.button(label= "Commencer l'exercice !!", style= discord.ButtonStyle.blurple)
     async def button_callback(self, interaction: discord.Interaction, bouton):
         await interaction.user.send(f"Tu m'as testé !")
-        await interaction.response.send_message(f"Bouton testé par {interaction.user.name}")
+        await interaction.response.send_message(f"{interaction.user.name} à fait cet Exo")
