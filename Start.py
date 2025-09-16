6bb05d17542a49bbffe6ac6f447fb34970e5b7c4
Julosse27 <lateurte.jules@gmail.com>
2025-09-16 19:37:49 +0200

    Update Start.py

--- Start.py
+++ Start.py
@@ -6,22 +6,21 @@ from keep_alive import keep_alive
 token = os.getenv("DISCORD_TOKEN")
 
 class Le_bot(commands.Bot):
-    async def setup_hook(self) -> None:
+    async def setup_hook(self):
+        self.remove_command("Exos_select")
         for extension in ["Modération", "Tests", "Tutos_cog"]:
             await self.load_extension(f"Cogs.{extension}")
 
+    async def on_ready(self):
+        print(f"Connecté en tant que {bot.user}")
+        synced = await bot.tree.sync()
+        print(f'{len(synced)} commande(s) syncronisée(s)')
+
 intents = discord.Intents.all()
 
-bot = Le_bot(command_prefix= "$", description= "Le bot qui sers à tout et à rien !!!", intents=intents)
+bot = Le_bot(command_prefix= "$", description= "Le bot qui sers à tout et à rien !!!", intents=intents, owner_id= 948981926264467466)
 
 keep_alive()
-
-@bot.event
-async def on_ready():
-    print(f"Connecté en tant que {bot.user}")
-    synced = await bot.tree.sync()
-    print(f'{len(synced)} commande(s) syncronisée(s)')
-
 
 bot.run(token= token)
 
