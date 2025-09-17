eb05a8be368a80b0d0eecb947c679be0529d285b
Julosse27 <lateurte.jules@gmail.com>
2025-09-17 18:19:15 +0200

    Update Commands_stock.py

--- Stocks/Commands_stock.py
+++ Stocks/Commands_stock.py
@@ -1,14 +1,15 @@
 from discord.ext import commands
 
-def check_me(*ids):
+async def check_me(*ids):
         
-        def predicate(ctx):
-            for role in ctx.author.roles:
-                if role.id in ids:
+        async def predicate(ctx: commands.Context):
+            for id in ids:
+                if ctx.author.get_role(id) != None:
                     return True
             else:
+                await ctx.send(content= "Vous n'avez pas les permisions requises !!!")
                 return False
-        
 
         return commands.check(predicate)
+
 
