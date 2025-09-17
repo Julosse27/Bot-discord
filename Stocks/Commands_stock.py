8c5d441eb76f96616f9ff8300a833abffbf2867d
Julosse27 <lateurte.jules@gmail.com>
2025-09-17 17:38:38 +0200

    Update Commands_stock.py

--- Stocks/Commands_stock.py
+++ Stocks/Commands_stock.py
@@ -7,8 +7,8 @@ def check_me(*ids):
                 if role.id in ids:
                     return True
             else:
-                ctx.send(content = "Vous n'avez pas les permisions requises")
                 return False
         
 
         return commands.check(predicate)
+
