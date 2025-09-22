27ffe40b1bfbf30efc35e15034421f609a31f4b3
Julosse27 <lateurte.jules@gmail.com>
2025-09-22 20:46:30 +0200

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -7,20 +7,13 @@ app = Flask("")
 def home():
     return "Le Bot est en ligne !"
 
-@app.route("/Test.txt")
-def render():
-    return open("Test_txt.txt", "r")
-
-@app.route("/bot.py")
-def affiche():
-    return open("Bot.py", "r")
-
 def run():
     app.run(host= "0.0.0.0", port= 8080)
 
 def keep_alive():
     t = Thread(target= run)
     t.start()
+
 
 
 
