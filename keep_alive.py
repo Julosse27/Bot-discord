5906a7176c9e0d971e9b2e073f1a7a3b0b1156d3
Julosse27 <lateurte.jules@gmail.com>
2025-09-28 18:46:17 +0200

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -24,9 +24,17 @@ def a():
 def b():
     return recup_fichier("Kenji_Battle/V2/architecture.txt")
 
+@app.route("/Kenji_Battle/V2/Données.sq3")
+def c():
+    return recup_fichier("Kenji_Battle/V2/Données.sq3")
+
+@app.route("/Kenji_Battle/V2/kenji_battle_ressources.pyxres")
+def d():
+    return recup_fichier("Kenji_Battle/V2/Kenji_Battle_Ressource.pyxres")
+
 def run():
     app.run(host= "0.0.0.0", port= 8080)
 
 def keep_alive():
     t = Thread(target= run)
-    t.start()   
+    t.start()
