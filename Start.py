a9890b3e3131e8d769dcb133334d4b67a444452d
Julosse27 <lateurte.jules@gmail.com>
2025-10-06 07:21:29 +0200

    Update Start.py

--- Start.py
+++ Start.py
@@ -91,6 +91,8 @@ class Le_bot(commands.Bot):
         print(f"Connecté en tant que {bot.user}")
         synced = await bot.tree.sync()
         print(f'{len(synced)} commande(s) syncronisée(s)')
+        if len(synced) == 1:
+            raise Exception("Il manque une commande")
 
 intents = discord.Intents.all()
 
@@ -99,4 +101,5 @@ bot = Le_bot(command_prefix= "$", description= "Le bot qui sers à tout et à ri
 keep_alive()
 
 bot.run(token= token)
+
 
