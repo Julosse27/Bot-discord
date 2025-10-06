7f6c6ff4134586e7374ec7db57cad8fb5f448b20
Julosse27 <lateurte.jules@gmail.com>
2025-10-06 07:14:01 +0200

    Update Modération.py

--- Cogs/Modération.py
+++ Cogs/Modération.py
@@ -2,10 +2,17 @@ import discord
 from discord.ext import commands
 from Stocks.Commands_stock import check_me
 
+
 class Modération(commands.Cog):
+    def slashs_commands(self):
+        @self.bot.tree.command(name= "re_test", description= "Envois un message 'test'")
+        async def test_slash(interaction: discord.Interaction):
+            await interaction.response.send_message(content= "test")
+
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.description = "Cog spécialisé dans les commandes de moderations."
+        self.slashs_commands()
 
     @commands.command(name= "message", aliases= ["mes", "ping"], description= "Envois un message à qui tu veut.", brief= "Envois un mp.")
     @check_me(1417199810099937411, 1382302424366186516, 1382303940921659412, 1382455975549599854)
