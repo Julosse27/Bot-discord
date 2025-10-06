f3e21f504c2286ba453d67589be61b89aee12a6b
Julosse27 <lateurte.jules@gmail.com>
2025-10-06 22:04:39 +0200

    Update Modération.py

--- Cogs/Modération.py
+++ Cogs/Modération.py
@@ -9,7 +9,7 @@ class Modération(commands.Cog):
         self.description = "Cog spécialisé dans les commandes de moderations."
 
     @app_commands.command(name= "kick", description= "Kick un membre pour une raison(optionel)")
-    @check_me(1382302424366186516, 1382303940921659412, 1382455975549599854)
+    @app_commands.checks.has_any_role(1410343531402367006, 1382302424366186516, 1382303940921659412, 1382455975549599854)
     async def test(self, interaction: discord.Interaction, member: discord.Member, reason: str | None = None):
         # await member.kick(reason= reason)
         if reason == None:
@@ -31,4 +31,3 @@ class Modération(commands.Cog):
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Modération(bot))
-
