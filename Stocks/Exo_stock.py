54d40c4b5d10d7c7004a232478189a8c0e11c1ab
Julosse27 <lateurte.jules@gmail.com>
2025-10-10 19:52:27 +0200

    Update Exo_stock.py

--- Stocks/Exo_stock.py
+++ Stocks/Exo_stock.py
@@ -1,15 +1,31 @@
 import discord
-from time import sleep
 
-class Tuto_1(discord.ui.View):
-    def __init__(self):
-        super().__init__(timeout = None)
+class Question(discord.ui.Button):
+    def __init__(self, label: str, nb: int):
+        self.rep = False
+        self.nb = nb
+        super().__init__(style= discord.ButtonStyle.blurple, label= label)
 
-    @discord.ui.button(label= "Commencer l'exercice !!", style= discord.ButtonStyle.blurple)
-    async def button_callback(self, interaction: discord.Interaction, bouton):
-        await interaction.response.defer()
-        sleep(5)
-        await interaction.user.send(f"Tu m'a testé")
-        await interaction.response.send_message(f"Regarde tes messages privés {interaction.user.name}!", ephemeral= True)
+    async def callback(self, interaction: discord.Interaction):
+        self.rep = True
+    
+class View_question(discord.ui.View):
+    def __init__(self, labels: list[str]):
+        super().__init__(timeout=500)
+        for i in range(len(labels)):
+            self.add_item(Question(labels[i], nb= i))
 
-exos = [Tuto_1]
+view = View_question(["test"])
+
+view.children
+
+exos = [
+    {'nom': "Types de variables", 
+     "questions": [
+        (View_question(["test", "test2"]),
+         "test",
+         0
+        )
+    ]
+    }
+]
