27629e7cad80ddf3b348f00a7c9fdf7ca33bbfb2
Julosse27 <lateurte.jules@gmail.com>
2025-10-10 20:13:47 +0200

    Update Tutos.py

--- Cogs/Tutos.py
+++ Cogs/Tutos.py
@@ -52,8 +52,7 @@ class Tutos(commands.Cog):
         for view, enonce, bonne_rep in questions:
             await interaction.edit_original_response(embed= discord.Embed(color= interaction.user.color, title= f"Question {questions.index((view, enonce, bonne_rep)) + 1}", description= enonce), view= view)
             
-            while not view.is_finished():
-                sleep(0.25)
+            await view.wait()
             
             for i in range(len(view.children)):
                 if view.children[i].rep:
@@ -74,7 +73,7 @@ class Tutos(commands.Cog):
                     else:
                         await interaction.edit_original_response(embed= discord.Embed(color= interaction.user.color, title= "Vous n'avez pas eu la bonne réponse.", description= f"La bonne réponse était {view.children[bonne_rep].label}.", timestamp= timestamp))
             
-            sleep(10)
+            sleep(15)
 
         
 async def setup(bot: commands.Bot):    
