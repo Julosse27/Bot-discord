d1c837b71f37ec43c081bdfaad434736ea8b8e84
Julosse27 <lateurte.jules@gmail.com>
2026-01-21 21:48:25 +0100

    ha je ne l'avait pas deja mit en ligne ?

--- creation_bd_stock.py
+++ creation_bd_stock.py
@@ -1,7 +1,7 @@
 import sqlite3 as sqlite
 from Stocks.File_stock.Recup_fichiers import recup_sqlite, recup_script_sql
 
-conn = sqlite.connect(recup_sqlite("donnees_stock_cafet.sq3"))
+conn = sqlite.connect(recup_sqlite("donnees_stock_cafet"))
 cur = conn.cursor()
 
 cur.executescript(recup_script_sql("script_création_cafet"))
